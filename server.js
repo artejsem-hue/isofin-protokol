cbc342240c52aafe0b8f14891743a3fb13d47826
artejsem-hue <artejsem@gmail.com>
2026-02-01 00:32:44 +0100

    Use path module for template file path

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const fs = require("fs");
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
+const path = require("path");
+
+const templatePath = path.join(__dirname, "template.docx");
+
 
 const app = express();
 app.use(express.json());
@@ -9,7 +13,7 @@ app.use(express.json());
 app.post("/generate", (req, res) => {
   const data = req.body;
 
-  const content = fs.readFileSync("template.docx", "binary");
+  const content = fs.readFileSync(templatePath, "binary");
   const zip = new PizZip(content);
   const doc = new Docxtemplater(zip);
 
